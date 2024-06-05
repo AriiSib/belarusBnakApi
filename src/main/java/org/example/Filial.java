f571f01964ddf08003818d35709612339dae0527
Artemii_Khokhlov <artemii.hhlov@gmail.com>
2024-06-06 00:16:12 +0600

    add readme and some refactoring

--- src/main/java/org/example/Filial.java
+++ src/main/java/org/example/Filial.java
@@ -8,7 +8,6 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 @NoArgsConstructor
 @Getter
